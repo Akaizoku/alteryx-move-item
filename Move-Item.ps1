bc000a778f95e264d438e117da0f429aa4fa11ee
Florian Carrier <Akaizoku@users.noreply.github.com>
2025-11-11 13:06:33 +0100

    MVP
    Initial MVP

---
+++ Move-Item.ps1
@@ -0,0 +1,33 @@
+# Parameters
+$Source      = '.\Input\sample.csv'
+$Destination = '.\Output\'
+$Log         = '.\log.csv'
+
+# Create target directory if required
+if (-not (Test-Path -LiteralPath $Destination)) {
+    try { $Null = New-Item -ItemType "Directory" -Path $Destination -Force -ErrorAction "Stop" } catch {}
+}
+
+# Move file
+try {
+    $Moved = Move-Item -LiteralPath $Source -Destination $Destination -PassThru -Force -ErrorAction "Stop"
+    $Entry = [PSCustomObject]@{
+        "Timestamp" = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+        "Source"    = $Source
+        "Status"    = "Moved"
+        "Target"    = $Moved.FullName
+    }
+}
+
+# Handle eventual errors
+catch {
+    $Entry = [PSCustomObject]@{
+        "Timestamp" = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+        "Source"    = $Source
+        "Status"    = "Error"
+        "Target"    = $PSItem.Exception.Message
+    }
+}
+
+# Export log
+Export-Csv -Path $Log -InputObject $Entry -NoTypeInformation -Append -Force
